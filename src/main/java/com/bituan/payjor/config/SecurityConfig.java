c6773fb52c75cef9c3359bcee70633312e20771b
bituan <113036994+bituann@users.noreply.github.com>
2025-12-09 21:53:33 +0100

    refactor: add rsakeys & jwt decoder

--- src/main/java/com/bituan/payjor/config/SecurityConfig.java
+++ src/main/java/com/bituan/payjor/config/SecurityConfig.java
@@ -6,11 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final RsaKeyProperties rsaKeys;
+
+    public SecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
@@ -31,5 +39,10 @@ public class SecurityConfig {
                 )
 //                .addFilterAfter(bearerTokenAuthFilter, BasicAuthenticationFilter.class);
                 .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder () {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
     }
 }
