0d5ab85ab9fe0ab4c5718541ab68faeaada020d5
bituan <113036994+bituann@users.noreply.github.com>
2025-12-09 20:05:38 +0100

    refactor: add user repository

---
+++ src/main/java/com/bituan/payjor/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bituan.payjor.repository;
+
+import com.bituan.payjor.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByGoogleUserId(String googleUserId);
+}
