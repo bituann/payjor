037ebd98705b7bd6ab8c39e1a8e5503ee4ea40ce
bituan <113036994+bituann@users.noreply.github.com>
2025-12-09 12:44:24 +0100

    initial commit

---
+++ src/main/java/com/bituan/payjor/PayjorApplication.java
@@ -0,0 +1,13 @@
+package com.bituan.payjor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayjorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayjorApplication.class, args);
+	}
+
+}
