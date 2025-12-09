34745434cfc032f1410a3789ae6b69a73a915fdd
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 00:35:40 +0100

    feat: add wallet entity

---
+++ src/main/java/com/bituan/payjor/model/entity/Wallet.java
@@ -0,0 +1,32 @@
+package com.bituan.payjor.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@Table(name = "wallets")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    @Column(scale = 2)
+    private double balance;
+
+    @Column(unique = true, precision = 10)
+    private long number;
+
+    @OneToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+}
