93a4a6f87bd66c42bd4d8fc1f0f36efe3ed1c65e
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 03:26:22 +0100

    feat: add wallet repo

---
+++ src/main/java/com/bituan/payjor/repository/WalletRepository.java
@@ -0,0 +1,13 @@
+package com.bituan.payjor.repository;
+
+import com.bituan.payjor.model.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+    Optional<Wallet> findByNumber(String number);
+}
