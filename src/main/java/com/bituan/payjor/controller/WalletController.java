7a0a47389e8b62f67694499e430fa468654b2b66
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 03:25:03 +0100

    feat: add tranfer endpoint

---
+++ src/main/java/com/bituan/payjor/controller/WalletController.java
@@ -0,0 +1,26 @@
+package com.bituan.payjor.controller;
+
+import com.bituan.payjor.model.request.WalletTransferRequest;
+import com.bituan.payjor.model.response.ApiResponse;
+import com.bituan.payjor.model.response.wallet.WalletTransferResponse;
+import com.bituan.payjor.service.wallet.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallet")
+@RequiredArgsConstructor
+public class WalletController {
+    private final WalletService walletService;
+
+    @PostMapping("/transfer")
+    public ResponseEntity<ApiResponse<?>> transfer (WalletTransferRequest request) {
+
+        ApiResponse<WalletTransferResponse> response = new ApiResponse<>(HttpStatus.OK.value(), walletService.transfer(request));
+        return ResponseEntity.ok(response);
+    }
+}
