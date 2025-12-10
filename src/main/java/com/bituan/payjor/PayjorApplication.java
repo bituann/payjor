fcbcb12df7646faad9a5ef896952cc95812ced62
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 04:18:06 +0100

    refactor: add config for rsa

--- src/main/java/com/bituan/payjor/PayjorApplication.java
+++ src/main/java/com/bituan/payjor/PayjorApplication.java
@@ -1,9 +1,12 @@
 package com.bituan.payjor;
 
+import com.bituan.payjor.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class PayjorApplication {
 
 	public static void main(String[] args) {
