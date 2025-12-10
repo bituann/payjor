09855edc90dd0c1b848a3f55740f53793a6c8600
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 08:11:03 +0100

    refactor: add apikey repository

---
+++ src/main/java/com/bituan/payjor/repository/ApiKeyRepository.java
@@ -0,0 +1,11 @@
+package com.bituan.payjor.repository;
+
+import com.bituan.payjor.model.entity.ApiKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
+}
--- src/main/java/com/bituan/payjor/repository/UserRepository.java
+++ src/main/java/com/bituan/payjor/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.bituan.payjor.repository;
 
 import com.bituan.payjor.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByGoogleUserId(String googleUserId);
     boolean existsByEmail(String email);
