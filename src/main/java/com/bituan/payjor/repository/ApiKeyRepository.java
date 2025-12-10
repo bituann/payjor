7250daf278ca223ab160845f17648a5cc332120a
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 10:33:56 +0100

    refactor: modify api key repo

--- src/main/java/com/bituan/payjor/repository/ApiKeyRepository.java
+++ src/main/java/com/bituan/payjor/repository/ApiKeyRepository.java
@@ -4,8 +4,10 @@ import com.bituan.payjor.model.entity.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
+    Optional<ApiKey> findByKey(String key);
 }
