e041f11a4f634dde648241aa57ecf5fa43717a1c
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 08:41:23 +0100

    docs: add swagger docs to main class

--- src/main/java/com/bituan/payjor/PayjorApplication.java
+++ src/main/java/com/bituan/payjor/PayjorApplication.java
@@ -1,9 +1,40 @@
 package com.bituan.payjor;
 
 import com.bituan.payjor.config.RsaKeyProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "PayJor"
+                ),
+                description = "Wallet App",
+                title = "PayJor Documentation",
+                version = "1.0"
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
