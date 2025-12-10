2ad9390f2a20e920a4101ef6f5f6492f12b25be4
bituan <113036994+bituann@users.noreply.github.com>
2025-12-10 12:41:49 +0100

    config: add spring configuration class for paystack

---
+++ src/main/java/com/bituan/payjor/config/PayStackConfig.java
@@ -0,0 +1,28 @@
+package com.bituan.payjor.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class PayStackConfig {
+
+    @Value("${paystack.secret-key}")
+    private String secretKey;
+
+    @Value("${paystack.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public WebClient payStackClient() {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + secretKey)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
