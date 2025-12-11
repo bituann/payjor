c75b5155ad2663e93d88a1f6d9408de0868337ca
bituan <113036994+bituann@users.noreply.github.com>
2025-12-11 01:56:08 +0100

    feat: make api-key filter mirroe jwk filter

--- src/main/java/com/bituan/payjor/config/ApiKeyAuthenticationFilter.java
+++ src/main/java/com/bituan/payjor/config/ApiKeyAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -37,15 +38,19 @@ public class ApiKeyAuthenticationFilter extends OncePerRequestFilter {
             if (matchedKey.isPresent()) {
                 ApiKey apiKey = matchedKey.get();
 
+                CustomUserDetails userDetails = new CustomUserDetails(apiKey.getOwner());
+
                 // Build Authentication object
                 UsernamePasswordAuthenticationToken auth =
                         new UsernamePasswordAuthenticationToken(
-                                new CustomUserDetails(apiKey.getOwner()),
+                                userDetails,
                                 null,
                                 apiKey.getPermissions().stream()
                                         .map(permission -> new SimpleGrantedAuthority("ROLE_" + permission.name()))
                                         .toList()
                         );
+
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(auth);
             } else {
