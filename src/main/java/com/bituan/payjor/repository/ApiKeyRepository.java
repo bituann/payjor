0e8b52cddcd8b26e11d41ef3022af93b85d2d269
bituan <113036994+bituann@users.noreply.github.com>
2025-12-11 12:56:43 +0100

    refactor: add new query to api keys repo

--- src/main/java/com/bituan/payjor/repository/ApiKeyRepository.java
+++ src/main/java/com/bituan/payjor/repository/ApiKeyRepository.java
@@ -1,13 +1,16 @@
 package com.bituan.payjor.repository;
 
 import com.bituan.payjor.model.entity.ApiKey;
+import com.bituan.payjor.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
     Optional<ApiKey> findByKey(String key);
+    List<ApiKey> findAllByOwner(User user);
 }
